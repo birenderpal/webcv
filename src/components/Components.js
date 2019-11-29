a8311d26c5d457da7fa0a00306bfd713352ac28f
Birender Pal <birender.pal@gmail.com>
2019-11-29 14:45:28 +1300

    modified components

--- src/components/Components.js
+++ src/components/Components.js
@@ -9,85 +9,85 @@ const StyledButton = styled.button`${props => props.css}`
 const StyledImage = styled.img`${props => props.css}`
 const StyledClickImage = styled.a`${props => props.css}`
 
-export const Grid = (props)=>{
-    const{
+export const Grid = (props) => {
+    const {
         style,
         id,
-        children                
+        children
     } = props
-    const gridClasses = classnames("page-row",props.class)
-    
-        return style && style.length > 1?(<StyledDiv css={style} className={gridClasses} id={id}>
-            {children}
-        </StyledDiv>)
+    const gridClasses = classnames("page-row", props.class)
+
+    return style && style.length > 1 ? (<StyledDiv css={style} className={gridClasses} id={id}>
+        {children}
+    </StyledDiv>)
         :
         (<div className={gridClasses} id={id}>
             {children}
-        </div>)    
+        </div>)
 }
 
-export const Button = props =>{
+export const Button = props => {
     const {
         style,
         text,
-        url}=props
-    return style && style.length > 1?
+        url } = props
+    return style && style.length > 1 ?
         (<StyledButton css={style} className={props.class}>
-            {url?<a href={url}>{text}</a>:text}
-            
-            </StyledButton>
-            )
+            {url ? <a href={url}>{text}</a> : text}
+
+        </StyledButton>
+        )
         :
         <button className={props.class}>
-            {url?<a href={url}>{text}</a>:text}
+            {url ? <a href={url}>{text}</a> : text}
         </button>
 }
 
-const contactMe =(e)=>{
+const contactMe = (e) => {
     const a = e.currentTarget
     if (a.hash !== "") {
         e.preventDefault();
         var hash = a.hash;
 
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1500, function(){
+            scrollTop: $(hash).offset().top
+        }, 1500, function () {
         });
-            //let page_id = hash.replace("#","")
-            $(hash).children(".page-container").addClass('fade-in')
-    }                          
+        //let page_id = hash.replace("#","")
+        $(hash).children(".page-container").addClass('fade-in')
+    }
 
 }
 
 
-export const ContactButton = props =>{
+export const ContactButton = props => {
     const {
         contactID,
     } = props
-    return(
+    return (
         <a className="bp-btn-hero bp-upper" href={contactID} onClick={contactMe}>contact</a>
     )
 }
 
-export const Column = (props) =>{
-    const{
+export const Column = (props) => {
+    const {
         style,
-        children                
+        children
     } = props
     const gridClasses = classnames(props.class)
-           return (style.length && style) ?
-            (
-                <StyledDiv css={style} className={gridClasses}>
-                    {children}
-                </StyledDiv>)
-                :
-                <div className={gridClasses}>
-                    {children}        
-                </div>
-    
+    return (style.length && style) ?
+        (
+            <StyledDiv css={style} className={gridClasses}>
+                {children}
+            </StyledDiv>)
+        :
+        <div className={gridClasses}>
+            {children}
+        </div>
+
 }
 
-export const Image = (props) =>{
+export const Image = (props) => {
     const {
         style,
         image,
@@ -95,22 +95,22 @@ export const Image = (props) =>{
         height,
         clickable,
         onclick
-    }=props
-    
-    const imageClass = classnames(props.class)    
-    return (clickable?
-            
-            (<StyledClickImage css={style} className={imageClass} href={onclick} target="_blank" rel="noopener noreferrer" 
-            style={{backgroundImage: `url(${image.path})`}}/>)                     
-            :
-            <StyledImage css={style} className={imageClass} src={image.path} height={image.height} width={image.width}/>        
-        )
+    } = props
+
+    const imageClass = classnames(props.class)
+    return (clickable ?
+
+        (<StyledClickImage css={style} className={imageClass} href={onclick} target="_blank" rel="noopener noreferrer"
+            style={{ backgroundImage: `url(${image.path})` }} />)
+        :
+        <StyledImage css={style} className={imageClass} src={image.path} height={image.height} width={image.width} />
+    )
 }
-export const Heading = props =>{
+export const Heading = props => {
     const {
-            tag,
-            text
-            } =props
+        tag,
+        text
+    } = props
     const headerClasses = classnames(props.class)
     const TagName = tag || 'p'
     return (
@@ -118,56 +118,56 @@ export const Heading = props =>{
     )
 }
 
-export const Divider = props =>{
-    const {        
+export const Divider = props => {
+    const {
         style
     } = props
     return (style && style.length > 1 ?
-         <StyledDiv css={style} className={props.class}/>:<div className={props.class}/>            
+        <StyledDiv css={style} className={props.class} /> : <div className={props.class} />
     )
 }
 
-export const Header = props =>{
+export const Header = props => {
     const {
-        id,    
-        text,        
+        id,
+        text,
         style,
         title,
         tag,
         subtitle,
         subtext,
         quote,
-        author}=props
+        author } = props
     const headerClasses = classnames(props.class)
     const TagName = tag || 'p'
-    const SubTitle = subtitle && subtitle.length > 0 ? <p className="page-header-subtitle">"{subtitle}"</p>:null
-    const SubText = subtext && subtext.length > 0 ? <p className="page-header-subtext">"{subtext}"</p>:null
-    const Quote = quote && quote.length > 0 ?<p className="quote">"{quote}"<br/>{author&&author.length>0?`- ${author} -`:null}
-</p>:null
+    const SubTitle = subtitle && subtitle.length > 0 ? <p className="page-header-subtitle">"{subtitle}"</p> : null
+    const SubText = subtext && subtext.length > 0 ? <p className="page-header-subtext">"{subtext}"</p> : null
+    const Quote = quote && quote.length > 0 ? <p className="quote">"{quote}"<br />{author && author.length > 0 ? `- ${author} -` : null}
+    </p> : null
     return (
         <div>
             <StyledDiv css={style} id={id}
                 className={headerClasses}>
-                {title}                
+                {title}
             </StyledDiv>
             {SubTitle}
             {Quote}
-            {SubText}            
+            {SubText}
         </div>
     )
 }
 
 
-export const TextComponent = props =>{
+export const TextComponent = props => {
     const {
-        id,        
+        id,
         text,
         style
     } = props
     const textClasses = classnames(props.class)
     return (
         <StyledDiv css={style} id={id} className={textClasses}
-            dangerouslySetInnerHTML={{__html:text}}>
+            dangerouslySetInnerHTML={{ __html: text }}>
         </StyledDiv>
     )
 }
@@ -176,10 +176,10 @@ export class ContactForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {}
-        this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    handleSubmit(e){
-        if (this.state.message.length < 2 || this.state.name.length < 1){
+    handleSubmit(e) {
+        if (this.state.message.length < 2 || this.state.name.length < 1) {
             alert(`Sorry, can't send an incomplete message, Please provide all details`);
             e.preventDefault();
 
@@ -188,16 +188,16 @@ export class ContactForm extends React.Component {
             alert(`One of email or phone is required`);
             e.preventDefault();
         } else {
-            let message = {"message":this.state.message,"name":this.state.name,"email":this.state.email,"phone":this.state.phone}
+            let message = { "message": this.state.message, "name": this.state.name, "email": this.state.email, "phone": this.state.phone }
             fetch(this.props.apiGatewayURL,
-                    {
-                        method: 'POST',
-                        body: JSON.stringify(message)
-                    }
-                )
-            .then(response => response.json())
+                {
+                    method: 'POST',
+                    body: JSON.stringify(message)
+                }
+            )
+                .then(response => response.json())
             alert('Message has been sent');
-            this.setState({name:"",email:"",message:"",phone:""})
+            this.setState({ name: "", email: "", message: "", phone: "" })
             e.preventDefault();
         }
     }
@@ -237,13 +237,13 @@ export class ContactForm extends React.Component {
                         onChange={(e) => this.setState({ message: e.currentTarget.value })} />
                 </TextField>
                 <h1>
-                    <button 
+                    <button
                         className="bp-btn contact-btn">
                         <span className="bp-upper">
-                        Submit
+                            Submit
                         </span>
-                        
-                </button>
+
+                    </button>
                 </h1>
             </form>
         )
